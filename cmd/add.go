08f2516e3817a6164a095e42b23d316057af82a1
archeopternix <39010055+archeopternix@users.noreply.github.com>
2021-01-11 13:15:22 +0100

    add command

--- cmd/add.go
+++ cmd/add.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,32 +21,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// createCmd represents the create command
-var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "Creates a new application with a default landingpage",
-	Long: `generates the basic logic to show a generic landing page, a config 
-	file containing basic information, echo router with a default route '/', `,
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		for _, arg := range args {
-			fmt.Println(arg)
-		}
-		fmt.Println("create called")
+		fmt.Println("add called")
 	},
 }
 
 func init() {
+	rootCmd.AddCommand(addCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// createCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	createCmd.Flags().StringVarP(&Application, "application", "a", "", "application name - needed when no config file in place")
-	createCmd.Flags().StringVarP(&Path, "path", "p", "", "target path of the generated application - needed when no config file in place")
-
-	rootCmd.AddCommand(createCmd)
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
